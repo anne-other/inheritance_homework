64012d5801162ba810b70e87b21d389dec02d3f8
anne-other <anne.c.ince@gmail.com>
2018-10-01 18:54:03 +0100

    Director has name

--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -1,5 +1,8 @@
 import Staff.Management.Director;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class DirectorTest {
 
@@ -8,5 +11,10 @@ public class DirectorTest {
     @Before
     public void setUp() throws Exception {
         director = new Director("Tony Stark", "5", 5000000.00, "Ironing");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Tony Stark", director.getName());
     }
 }
