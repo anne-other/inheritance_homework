d41423cc0bbe0b9ac1c8188eb9ad61d765e8ec35
anne-other <anne.c.ince@gmail.com>
2018-10-01 16:12:30 +0100

    Developer has name and NI Number

---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,26 @@
+import Staff.TechStaff.Developer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class DeveloperTest {
+    
+    Developer developer;
+
+    @Before
+    public void setUp() throws Exception {
+        developer = new Developer("Bruce Banner", "H01K", 20.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bruce Banner", developer.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("H01K", developer.getnINumber());
+    }
+}
