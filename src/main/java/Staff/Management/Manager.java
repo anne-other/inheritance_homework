ccbb64940483051658b3a724ff4f088261392559
anne-other <anne.c.ince@gmail.com>
2018-10-01 15:59:24 +0100

    Creating manager subclass which inherits from employee

---
+++ src/main/java/Staff/Management/Manager.java
@@ -0,0 +1,10 @@
+package Staff.Management;
+
+import Staff.Employee;
+
+public class Manager extends Employee {
+
+    public Manager(String name, String nINumber, double salary) {
+        super(name, nINumber, salary);
+    }
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,41 @@
+import Staff.Management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void setUp() throws Exception {
+        manager = new Manager("Pepper Potts","AV3N63R", 1000000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Pepper Potts", manager.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("AV3N63R", manager.getnINumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(1000000.00, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSallary() {
+        manager.raiseSalary(100.00);
+        assertEquals(1000100.00, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonus() {
+        assertEquals(10000.00, manager.paybonus(), 0.01);
+    }
+}
